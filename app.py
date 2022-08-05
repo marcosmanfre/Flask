f9ac54a47aae89d6bb01601e1ea6cc38a3ceb274
Marcos A. Manfre <marcosmanfre@hotmail.com>
2022-08-05 10:43:27 -0300

    atualizar

--- app.py
+++ app.py
@@ -6,16 +6,40 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<p> Bom dia</p>"
+     return render_template('index.html')
 
-@app.route("/sobre")
-def sobre():
-    return "<h1> portal de vendas </h1>"
+@app.route("/cadastro/usuario")
+def usuario():
+    return render_template('usuario.html', titulo="Cadastro de Usuario")
 
-@app.route("/teste")
-def teste():
-    return render_template('index.html')
+@app.route("/cadastro/anuncios")
+def anuncio():
+    return render_template('anuncio.html')
 
-@app.route("/user/<username>")
-def username(username):
-    return render_template('user.html',username=username)
+@app.route("/anuncios/pergunta")
+def pergunta():
+    return render_template('pergunta.html')
+
+
+@app.route("/anuncios/compra")
+def compra():
+    print("anuncio comprado")
+    return "<h4> comprado </h4>"
+
+@app.route("/anuncios/favoritos")
+def favoritos():
+    print("favorito inserido")
+    return ""
+
+@app.route("/config/categoria")
+def categoria():
+    return render_template('categoria.html')
+
+@app.route("/relatorios/vendas")
+def relVendas():
+    return render_template('relVendas.html')
+
+@app.route("/relatorios/compras")
+def relCompras():
+    return render_template('relCompras.html')
+
