86cf33970bf72cb919e695bf330e3c70a1ac2fe9
Marcos A. Manfre <marcosmanfre@hotmail.com>
2022-08-10 11:26:19 -0300

    atualização

--- app.py
+++ app.py
@@ -26,6 +26,39 @@ class Usuario(db.Model):
         self.senha = senha
         self.end = end
 
+class Categoria(db.Model):
+    __tablename__ = "categoria"
+    id = db.Column('cat_id', db.Integer, primary_key=True)
+    nome = db.Column('cat_nome', db.String(256))
+    desc = db.Column('cat_desc', db.String(256))
+
+    def __init__ (self, nome, desc):
+        self.nome = nome
+        self.desc = desc
+
+class Anuncio(db.Model):
+    __tablename__ = "anuncio"
+    id = db.Column('anu_id', db.Integer, primary_key=True)
+    nome = db.Column('anu_nome', db.String(256))
+    desc = db.Column('anu_desc', db.String(256))
+    qtd = db.Column('anu_qtd', db.Integer)
+    preco = db.Column('anu_preco', db.Float)
+    cat_id = db.Column('cat_id',db.Integer, db.ForeignKey("categoria.cat_id"))
+    usu_id = db.Column('usu_id',db.Integer, db.ForeignKey("usuario.usu_id"))
+
+    def __init__ (self, nome, desc, qtd, preco, cat_id, usu_id):
+        self.nome = nome
+        self.desc = desc
+        self.qtd = qtd
+        self.preco = preco
+        self.cat_id = cat_id
+        self.usu_id = usu_id
+
+
+@app.errorhandler(404)
+def paginanaoencontrada(error):
+    return render_template('paginanaoencontrada.html')
+
 @app.route("/")
 def index():
      return render_template('index.html')
@@ -35,24 +68,60 @@ def cadastros():
     return render_template('cadastros.html')
 
 @app.route("/cadastros/usuario")
-def cadusuario():
+def usuario():
     return render_template('usuario.html', usuarios = Usuario.query.all(), titulo="Cadastro de Usuário")
 
 
-@app.route("/cadastros/caduser", methods=['POST'])
-def caduser():
+@app.route("/usuario/criar", methods=['POST'])
+def criarusuario():
     usuario = Usuario(request.form.get('user'),request.form.get('email'),request.form.get('passwd'),request.form.get('end'))
     db.session.add(usuario)
     db.session.commit()
-    return redirect(url_for('cadusuario'))
+    return redirect(url_for('usuario'))
 
-@app.route("/cadastros/anuncios")
+@app.route("/usuario/detalhar/<int:id>")
+def buscarusuario(id):
+    usuario = Usuario.query.get(id)
+    return usuario.nome
+
+@app.route("/usuario/editar/<int:id>", methods=['GET','POST'])
+def editarusuario(id):
+    usuario = Usuario.query.get(id)
+    if request.method == 'POST':
+        usuario.nome = request.form.get('user')
+        usuario.email = request.form.get('email')
+        usuario.senha = request.form.get('passwd')
+        usuario.end = request.form.get('end')
+        db.session.add(usuario)
+        db.session.commit()
+        return redirect(url_for('usuario'))
+    
+    return render_template('eusuario.html', usuario = usuario, titulo="Usuario")
+
+@app.route("/usuario/deletar<int:id>")
+def deletarusuario(id):
+    usuario = Usuario.query.get(id)
+    db.session.delete(usuario)
+    db.session.commit()
+    return redirect(url_for('usuario'))
+
+
+@app.route("/cadastros/anuncio")
 def anuncio():
-    return render_template('anuncio.html')
+    return render_template('anuncio.html', anuncios = Anuncio.query.all(), categorias = Categoria.query.all(), titulo="Anuncio")
 
 @app.route("/anuncios")
 def anuncios():
     return render_template('anuncios.html')
+
+
+@app.route("/anuncio/novo", methods=['POST'])
+def novoanuncio():
+    anuncio = Anuncio(request.form.get('nome'), request.form.get('desc'),request.form.get('qtd'),request.form.get('preco'),request.form.get('cat'),request.form.get('uso'))
+    db.session.add(anuncio)
+    db.session.commit()
+    return redirect(url_for('anuncio'))
+
 
 @app.route("/anuncios/pergunta")
 def pergunta():
@@ -74,7 +143,14 @@ def config():
 
 @app.route("/config/categoria")
 def categoria():
-    return render_template('categoria.html')
+    return render_template('categoria.html', categorias = Categoria.query.all(), titulo='Categoria')
+
+@app.route("/categoria/novo", methods=['POST'])
+def novacategoria():
+    categoria = Categoria(request.form.get('nome'), request.form.get('desc'))
+    db.session.add(categoria)
+    db.session.commit()
+    return redirect(url_for('categoria'))
 
 @app.route("/relatorios")
 def relatorios():
